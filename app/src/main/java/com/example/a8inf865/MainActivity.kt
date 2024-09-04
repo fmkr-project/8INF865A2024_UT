234845fd4bf17de338f249e6b61e5c68160ef50d
thay <91734800+fmkr-project@users.noreply.github.com>
2024-09-04 13:14:03 -0400

    Convert to Whitesmiths

--- app/src/main/java/com/example/a8inf865/MainActivity.kt
+++ app/src/main/java/com/example/a8inf865/MainActivity.kt
@@ -5,43 +5,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a8inf865.ui.theme._8INF865Theme as ExampleTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ExampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {}
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hej $name!",
-        modifier = modifier
-    )
+fun Greeting2(text: String, modifier: Modifier = Modifier)
+{
+    Text(text = text)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun BCPreview() {
+fun BCPreview()
+{
     ExampleTheme {
-        Greeting("Rick Astley")
+        Greeting2(text = "Lorem ipsum dolor sit amet")
     }
 }
