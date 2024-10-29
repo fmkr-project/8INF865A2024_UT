9cc5e18b0dfaf4fa6d1001b45ee043c3fe8da5ed
thay <91734800+fmkr-project@users.noreply.github.com>
2024-10-29 11:46:03 -0400

    Auto-generated commit

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -83,12 +83,13 @@ fun GameScreen(
         GameLayout(
             currentScrambledWord = gameUiState.currentScrambledWord,
             userGuess = gameViewModel.userGuess,
+            isGuessWrong = gameUiState.isGuessedWordWrong,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
                 .padding(mediumPadding),
             onUserGuessChanged = { guess -> gameViewModel.updateUserGuess(guess) },
-            onKeyboardDone = {}
+            onKeyboardDone = { gameViewModel.checkUserGuess() }
         )
         Column(
             modifier = Modifier
@@ -99,7 +100,7 @@ fun GameScreen(
 
             Button(
                 modifier = Modifier.fillMaxWidth(),
-                onClick = { }
+                onClick = { gameViewModel.checkUserGuess() }
             ) {
                 Text(
                     text = stringResource(R.string.submit),
@@ -138,6 +139,7 @@ fun GameStatus(score: Int, modifier: Modifier = Modifier) {
 @Composable
 fun GameLayout(
     onUserGuessChanged: (String) -> Unit,
+    isGuessWrong: Boolean,
     userGuess: String,
     onKeyboardDone: () -> Unit,
     currentScrambledWord: String,
@@ -185,8 +187,10 @@ fun GameLayout(
                     disabledContainerColor = colorScheme.surface,
                 ),
                 onValueChange = onUserGuessChanged,
-                label = { Text(stringResource(R.string.enter_your_word)) },
-                isError = false,
+                label = {
+                    if (isGuessWrong) Text(stringResource(R.string.wrong_guess))
+                    else Text(stringResource(R.string.enter_your_word)) },
+                isError = isGuessWrong,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
